8aaa8cd58cc1c68118e13cf4791569e159dd8fee
Edson Junior <edsonteixeira056@gmail.com>
2024-04-02 22:13:18 -0300

    add atributos project_receita

--- Palmirinha_app/models.py
+++ Palmirinha_app/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Categoria(models.Model):
     nome = models.CharField(max_length=30)
 
+
     def __str__(self):
         return self.nome
     
@@ -15,11 +16,15 @@ class Receita(models.Model):
         ('EX','Extremo')
     ]
     nome = models.CharField(max_length = 50)
-    #tempo_de_preparo = models.DecimalField(max_digits=2, decimal_digits=2)
+    tempo_de_preparo = models.TimeField(max_length=4, blank=False, null=True, help_text='Horas:Minutos')
     ingredientes = models.TextField(max_length=8000)
     modo_de_preparo = models.TextField(max_length=8000)
     dificuldade = models.CharField(max_length=10, choices= Dificuldade)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    creditos =models.CharField(max_length=30, blank=True, null=True,)
+    fonte =models.CharField(max_length=50, blank=True, null=True)
+    URL =models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.nome
+#HELP_TEXT
